1e879f19b9a8b144f4ff2feb4c16848c15c1fa1f
권남웅\mario <mario2368@naver.com>
2026-03-23 16:54:24 +0900

    [Mine] MyActor 클래스 리팩토링

--- Source/MyProject7/MyActor.cpp
+++ Source/MyProject7/MyActor.cpp
@@ -13,13 +13,13 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking~~~"));
 }
 
